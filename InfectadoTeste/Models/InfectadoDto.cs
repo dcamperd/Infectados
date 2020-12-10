5c07c66751c54f67e0dd9bc14c2f0f2e8588372c
Duda <Duda.git@gmail.com>
2020-12-10 10:03:02 -0300

    Add /Models/InfectadoDto.cs

--- InfectadoTeste/Models/InfectadoDto.cs
+++ InfectadoTeste/Models/InfectadoDto.cs
@@ -1,7 +1,14 @@
+using System;
+
 namespace InfectadoTeste.Models
 {
     public class InfectadoDto
     {
-        
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sexo { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }s
     }
 }
