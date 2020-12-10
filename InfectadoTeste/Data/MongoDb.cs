c8f7398b2a19507888469c4ea7256ab9a85a4407
Duda <Duda.git@gmail.com>
2020-12-10 10:01:40 -0300

    Add /Data/MongoDb.cs

--- InfectadoTeste/Data/MongoDb.cs
+++ InfectadoTeste/Data/MongoDb.cs
@@ -1,5 +1,36 @@
+using System;
+using InfectadoTeste.Data.Collections;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Driver;
+
 namespace InfectadoTeste.Data {
     public class MongoDb {
+        public IMongoDatabase Db { get; }
+
+        public MongoDb(IConfiguration configuration) {
+            try {
+                var settings = MongoClientSettings.FromUrl(new MongoUrl(configuration["ConnectionString"]));
+                var client = new MongoClient(settings);
+                Db = client.GetDatabase(configuration["NomeBanco"]);
+                MapClasses();
+            }
+            catch (Exception ex) {
+                throw new MongoException("Nao foi possivel conectar ao MongoDb", ex);
+            }
+        }
         
+        private void MapClasses() {
+            var convetionPack = new ConventionPack { new CamelCaseElementNameConvention() };
+            ConventionRegistry.Register("camelCase", convetionPack, t => true);
+
+            if(!BsonClassMap.IsClassMapRegistered(typeof(Infectado))) {
+                BsonClassMap.RegisterClassMap<Infectado>(i => {
+                    i.AutoMap();
+                    i.SetIgnoreExtraElements(true);
+                });
+            }
+        }
     }
 }
